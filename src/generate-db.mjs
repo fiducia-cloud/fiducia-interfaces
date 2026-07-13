487e470c45ab5851e8f6f3b1dc048fe067fbf408
alex-mills <alexander.d.mills@gmail.com>
2026-07-13 03:18:48 -0500

    Make generated interface builds reproducible

--- src/generate-db.mjs
+++ src/generate-db.mjs
@@ -152,7 +152,7 @@ edition = "2021"
 description = "Generated sqlx::FromRow row types for the fiducia.cloud Postgres planes (see fiducia-interfaces)."
 
 [dependencies]
-sqlx = { version = "0.8", default-features = false, features = ["postgres", "uuid", "chrono", "json", "macros"] }
+sqlx = { version = "0.9", default-features = false, features = ["postgres", "uuid", "chrono", "json", "macros"] }
 uuid = { version = "1", features = ["serde"] }
 chrono = { version = "0.4", features = ["serde"] }
 serde = { version = "1", features = ["derive"] }
